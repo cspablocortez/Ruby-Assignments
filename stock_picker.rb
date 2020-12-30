af1f9853742e02a443d6bb168e3549a2cbb32538
Pablo Cortez <cspablocortez@gmail.com>
2020-12-30 11:55:44 -0800

    Formatting & Rubocop linting

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,22 +2,21 @@
 # Author: Pablo Cortez
 # Date: 15 December 2020
 
-stocks = [17,3,6,9,15,8,6,1,10]
+stocks = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 
 def stock_picker(stocks)
-  days = Array.new(2) # => [nil, nil]
   sorted_stocks = stocks.sort
   min_index = stocks.index(sorted_stocks.min)
   max_index = stocks.index(sorted_stocks.max)
 
-  while min_index > max_index do
+  while min_index > max_index
     sorted_stocks.delete(stocks.min)
     sorted_stocks.delete(stocks.max)
     min_index = stocks.index(sorted_stocks.min)
     max_index = stocks.index(sorted_stocks.max)
   end
-  
-  days = [min_index, max_index]
+
+  [min_index, max_index]
 end
 
 p stock_picker(stocks)
