7a139e5654d5dbdda59ed3fe68a1802d6b015c31
Pablo Cortez <cspablocortez@gmail.com>
2021-01-02 05:19:55 -0800

    Refactoring

--- Projects/event_manager/lib/event_manager.rb
+++ Projects/event_manager/lib/event_manager.rb
@@ -1,34 +1,21 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-
-
 def clean_zipcode(zipcode)
-  # if zipcode.nil?
-  #   '00000'
-  # elsif zipcode.length < 5
-  #   zipcode.rjust(5, '0')
-  # elsif zipcode.length > 5
-  #   zipcode[0..4]
-  # else
-  #   zipcode
-  # end
   zipcode.to_s.rjust(5,"0")[0..4] # Coercion over questions
 end
 
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = ~
-  begin
   legislators = civic_info.representative_info_by_address(
     address: zipcode, 
     levels: 'country', 
     roles: ['legislatorUpperBody', 'legislatorLowerBody']
   )
   legislator_names = legislators.officials.map(&:name).join(", ")
-  rescue
+  rescue 
     "You can find your representatives by visiting: www.commoncause.org/take-action/find-elected-officials"
-  end
 end
 
 puts 'EventManager Initialized...'
