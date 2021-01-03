38e2eb44192518cbeca8cd943dc949af148e7206
Pablo Cortez <cspablocortez@gmail.com>
2021-01-03 02:29:04 -0800

    Create ERB template and write to its own HTML file for each event attendee

--- Projects/event_manager/lib/event_manager.rb
+++ Projects/event_manager/lib/event_manager.rb
@@ -26,11 +26,22 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read 'form_letter.erb'
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0] # Assign an ID for the attendee
   first_name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislator_names = legislators_by_zipcode(zipcode)
-  puts "#{first_name} #{zipcode} #{legislator_names}"
-end
+  form_letter = erb_template.result(binding)
+  
+  # Create an output folder
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
 
-# TODO: Iteration 4: Form Letters
+  # Save each form letter to a file based on the id of the attendee
+  File.open(filename, 'w') do | file |
+    file.puts form_letter
+  end
+end
